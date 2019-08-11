2a21f9c6a3e823a9e55bf0fd2424cc13234331b1
Ngat Do <ngat.do@powergatesoftware.com>
2019-08-11 18:02:40 +0700

    Create layout for improt and delete files and data

--- www/js/javascript.js
+++ www/js/javascript.js
@@ -1,31 +1,17 @@
 
-function changeIconHeader() {
-  var element = document.body;
-  if (element.classList) {
-    element.classList.toggle("header__icon--close");
-  } else {
-    // For IE9
-    var classes = element.className.split(" ");
-    var i = classes.indexOf("header__icon--close");
-
-    if (i >= 0)
-      classes.splice(i, 1);
-    else
-      classes.push("header__icon--close");
-    element.className = classes.join(" ");
-  }
-}
-
 function openContent(evt, tabName) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName("block-page");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName("sidebar__item");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" tab__item--active", "");
+    tablinks[i].className = tablinks[i].className.replace(
+      " sidebar__item--active",
+      ""
+    );
   }
   document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " tab__item--active";
+  evt.currentTarget.className += " sidebar__item--active";
 }
