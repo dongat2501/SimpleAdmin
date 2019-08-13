3d6016eb8f7d3b971942057955a42d7828af9cb5
Ngat Do <ngat.do@powergatesoftware.com>
2019-08-13 18:31:37 +0700

    Change Jquery

--- www/js/javascript.js
+++ www/js/javascript.js
@@ -18,42 +18,42 @@ $(document).ready(function() {
   $("input[type=radio][name=typeImport]").change(function() {
     console.log(this.value);
     if (this.value == "file") {
-      $(".file-content").removeClass('hidden');
-      $(".data-content").addClass('hidden');
+      $(".file-content").show();
+      $(".data-content").hide();
     } else if (this.value == "data") {
-         $(".file-content").addClass("hidden");
-        $(".data-content").removeClass("hidden");
+         $(".file-content").hide();
+        $(".data-content").show();
     }
   });
 
   $("input[type=radio][name=importMode]").change(function() {
     console.log(this.value);
     if (this.value == "overwrite") {
-      $(".import-warning-text").removeClass("hidden");
+      $(".import-warning-text").show();
     } else if (this.value == "add") {
-      $(".import-warning-text").addClass("hidden");
+      $(".import-warning-text").hide();
     }
   });
 
   $("input[type=radio][name=tableMode]").change(function() {
     console.log(this.value);
     if (this.value == "multi-table") {
-      $(".table-warning-text").removeClass("hidden");
-      $(".select-single-table").addClass("hidden");
+      $(".table-warning-text").show();
+      $(".select-single-table").hide();
     } else if (this.value == "single-table") {
-      $(".table-warning-text").addClass("hidden");
-      $(".select-single-table").removeClass("hidden");
+      $(".table-warning-text").hide();
+      $(".select-single-table").show();
     }
   });
 
    $("input[type=radio][name=deleteType]").change(function() {
      console.log(this.value);
      if (this.value == "deteleData") {
-       $(".delete-data").removeClass("hidden");
-       $(".delete-file").addClass("hidden");
+       $(".delete-data").show();
+       $(".delete-file").hide();
      } else {
-       $(".delete-data").addClass("hidden");
-       $(".delete-file").removeClass("hidden");
+       $(".delete-data").hide();
+       $(".delete-file").show();
      }
    });
 });
